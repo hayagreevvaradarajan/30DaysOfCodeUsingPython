2ef95466524b8232a59344a643205a37c56cd44b
Hayagreev <47315980+hayagreevchintu@users.noreply.github.com>
2021-05-23 17:49:40 +0530

    Update day9.py

--- day9.py
+++ day9.py
@@ -1,14 +1,18 @@
-#!/bin/python3
+#Stub code:
 import math
 import os
 import random
 import re
 import sys
+
+#Solution begins here:
 def factorial(n):
     if n == 0:
         return 1
     else:
         return n * int(factorial(n-1))
+    
+#Stub code:
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
     n = int(input())
