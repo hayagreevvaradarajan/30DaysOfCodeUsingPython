7bb26fb26cfc88bb62d10d47fbdb271cd0bd2415
Hayagreev <47315980+hayagreevchintu@users.noreply.github.com>
2021-05-23 17:47:49 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,13 +1,13 @@
+#Solution begins here:
 class Person:
     def __init__(self,initialAge):
-        # Add some more code to run some checks on initialAge
         if initialAge <0:
             self.age=0
             print("Age is not valid, setting age to 0.")
         else:
             self.age = initialAge
+    
     def amIOld(self):
-        # Do some computations in here and print out the correct statement to the console
         if self.age>=0 and self.age<13:
             print("You are young.")
         elif self.age >=13 and self.age<18:
@@ -16,9 +16,9 @@ class Person:
             print("You are old.")
            
     def yearPasses(self):
-        # Increment the age of the person in here
         self.age = self.age + 1
 
+#Stub code:
 t = int(input())
 for i in range(0, t):
     age = int(input())         
