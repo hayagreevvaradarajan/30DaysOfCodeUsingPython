39004fb62e6e9e6b8b6bffba148b6a68d2288bfe
Hayagreev <47315980+hayagreevchintu@users.noreply.github.com>
2021-05-23 17:46:01 +0530

    Update day2.py

--- day2.py
+++ day2.py
@@ -1,15 +1,18 @@
+#Stub code:
 import math
 import os
 import random
 import re
 import sys
 
+#Solution begins here:
 def solve(meal_cost, tip_percent, tax_percent):
     tip_cost = (meal_cost * tip_percent)/100
     tax_cost = (meal_cost * tax_percent)/100
     total_cost = meal_cost + tip_cost + tax_cost
     print(round(total_cost))
 
+#Stub code:
 if __name__ == '__main__':
     meal_cost = float(input())
     tip_percent = int(input())
